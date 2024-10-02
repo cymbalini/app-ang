e8ba3f8d88cf856aa245827b9a7122167cecf2d3
Krzysztof Nowak <992haslo992@gmail.com>
2024-10-02 08:28:04 +0200

    added data.ts

---
+++ src/app/data/contacts-data.ts
@@ -0,0 +1,5 @@
+export const contactsData = [
+    {id: 1, surname: 'Kowalski', firstname: 'Jan', city: 'Kraków', phoneNumber: '4435435', mail: 'jan@example.com'},
+    {id: 2, surname: 'Nowak', firstname: 'Anna', city: 'Warszawa', phoneNumber: '54435435', mail: 'anna@example.com'},
+    {id: 3, surname: 'Kowalska', firstname: 'Zofia', city: 'Gdańsk', phoneNumber: '784435435', mail: 'zofia@example.com'},
+  ]
