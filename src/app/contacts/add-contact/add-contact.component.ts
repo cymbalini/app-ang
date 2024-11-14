3f6860a0ec82ffa6757dcfa1d89da162b323250d
Krzysztof Nowak <992haslo992@gmail.com>
2024-11-14 09:32:04 +0100

    selected changes

--- src/app/contacts/add-contact/add-contact.component.ts
+++ src/app/contacts/add-contact/add-contact.component.ts
@@ -1,9 +1,7 @@
 import { Component } from '@angular/core';
-<<<<<<< HEAD
-import { FormBuilder,Validators } from '@angular/forms';
-=======
+
 import { FormBuilder, Validators } from '@angular/forms';
->>>>>>> 81d94246c15a617603ac1adcb767a48d95647e40
+
 
 @Component({
   selector: 'app-add-contact',
@@ -22,12 +20,8 @@ export class AddContactComponent {
     this.createFormModel();
   }
   createFormModel(){
-<<<<<<< HEAD
 
-    const pattern: string | RegExp = '^[A-ż]{2,10}$'
-=======
     const stringPattern: string | RegExp = '^[A-ż]{2,30}$';
->>>>>>> 81d94246c15a617603ac1adcb767a48d95647e40
 
     this.formModel = this.fb.group({
     surname: ['', [Validators.required,Validators.pattern(stringPattern)]],
@@ -35,27 +29,11 @@ export class AddContactComponent {
     city: ['',Validators.required,Validators.pattern(stringPattern)],
     phoneNumber: [''],
     email: ['']
-
-      surname: ['', Validators.required, Validators.pattern(pattern)],
-      name: ['', Validators.required, Validators.pattern(pattern)],
-      city: [''],
-      phoneNumber: [''],
-      email: ['']
-
     });
   }
-<<<<<<< HEAD
 
-  saveData(){
-      console.log(this.formModel.value);
-=======
   saveData() {
     console.log(this.formModel.value);
     this.formModel.reset(); // Reset form after save
-<<<<<<< HEAD
->>>>>>> 81d94246c15a617603ac1adcb767a48d95647e40
-=======
-    // adsa
->>>>>>> f5f11614e3c2ac7cb0d584ab22b91ab7394ba944
   }
 }
